e46a62dd8bdf7cfeab37beca89ba8833e76db442
jaggersb1 <101071422+jaggersb1@users.noreply.github.com>
2022-04-16 11:01:30 -0400

    Something happened during merging that removed a return statement in the ActionList method. Hopefully that is the only issue, it is fixed now.

--- src/csc439TeamPeacock/Player.java
+++ src/csc439TeamPeacock/Player.java
@@ -40,7 +40,7 @@ public class Player {
         //List of actions that a player can take on their turn, assuming they haven't already picked up a card.
         actionList.add("Draw from shoe");
         actionList.add("Draw from discard pile");
-
+        return actionList;
       
     }
 
